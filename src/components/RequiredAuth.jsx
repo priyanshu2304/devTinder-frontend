e380c068e7cc136ebb8e90b8436958a542f0d2af
priyanshu2304 <priyanshumodi1998@gmail.com>
2025-06-04 08:28:13 +0530

    Fix page reload issue

--- src/components/RequiredAuth.jsx
+++ src/components/RequiredAuth.jsx
@@ -1,15 +1,44 @@
 // components/RequireAuth.js
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
+import axios from '../utils/axiosInstance';
+import { addUser } from '../store/userSlice';
 
 const RequireAuth = ({ children }) => {
   const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const location = useLocation();
+  const [loading, setLoading] = useState(true);
 
-  const isAuthenticated = Boolean(user?.firstName);
+  const fetchUser = async () => {
+    try {
+      const resp = await axios.get('/profile/view');
+      dispatch(addUser(resp.data));
+    } catch (error) {
+      // not authenticated, do nothing here, will redirect below
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  if (!isAuthenticated) {
+  useEffect(() => {
+    if (!user?.firstName) {
+      fetchUser();
+    } else {
+      setLoading(false);
+    }
+  }, [user]);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <span className="loading loading-spinner loading-lg text-primary"></span>
+      </div>
+    );
+  }
+
+  if (!user?.firstName) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
