3b8229919f987229e798d3830902bfd95509c5df
priyanshu2304 <priyanshumodi1998@gmail.com>
2025-06-04 06:08:00 +0530

    added constant

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,8 +1,9 @@
 // src/utils/axiosInstance.js
 import axios from 'axios';
+import BASE_URL from './constants';
 
 const instance = axios.create({
-  baseURL: '/api',
+  baseURL: BASE_URL,
   withCredentials: true,
 });
 
