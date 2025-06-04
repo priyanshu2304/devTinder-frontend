34e80872efa223865088d2c6e801bbe93569a6dc
priyanshu2304 <priyanshumodi1998@gmail.com>
2025-06-04 06:17:41 +0530

    fix issue

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 // src/utils/axiosInstance.js
 import axios from 'axios';
-import BASE_URL from './constants';
+import { BASE_URL } from './constants';
 
 const instance = axios.create({
   baseURL: BASE_URL,
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,1 @@
-export default BASE_URL = '/api';
+export const BASE_URL = '/api';
